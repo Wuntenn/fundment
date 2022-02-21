221c143bb212417952bdce414514839d08760603
marcos.hannecke <marcos@fundment.com>
2022-02-21 15:20:30 +0000

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormOneComponent } from "./forms/form-one/form-one.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, FormOneComponent],
     }).compileComponents();
   });
 
@@ -22,12 +23,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-coding-test');
   });
 
-  test('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'angular-coding-test app is running!'
-    );
-  });
 });
